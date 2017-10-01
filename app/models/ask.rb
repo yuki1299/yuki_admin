882a949e248f627fa1e1aaac09f0ccd767ed169e
Vinicius <vinicius@loldesign.com.br>
2017-09-30 22:11:22 -0300

    Added enumerize to asks and public fields

--- app/models/ask.rb
+++ app/models/ask.rb
@@ -1,3 +1,6 @@
 class Ask < ApplicationRecord
   belongs_to :project
+  extend Enumerize
+
+  enumerize :ask_type, in: [:opened_ask, :closed_ask, :scale_zero_to_five]
 end
--- app/models/public.rb
+++ app/models/public.rb
@@ -1,3 +1,8 @@
 class Public < ApplicationRecord
   belongs_to :project
+  extend Enumerize
+
+  enumerize :gender, in: [:female, :male]
+  enumerize :schooling, in: [:fundamental, :medium, :superior, :post_graduated, :doctorated, :master]
+  enumerize :locale, in: [:north, :northeast, :midwest, :southeast, :south]
 end
