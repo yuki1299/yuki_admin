9cfa85c6e27843ce2d536d5e82d4dfff4b832e33
Yuki Finamore <yuki@loldesign.com.br>
2017-10-20 20:53:52 -0200

    Styling register user step 2

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,22 @@
 $(document).on('ready page:load', function() {
+  // Radio Button Unmark
   var allRadios = document.getElementsByName('re');
+  var booRadio;
+  var x = 0;
+  for(x = 0; x < allRadios.length; x++){
+
+      allRadios[x].onclick = function() {
+          if(booRadio == this){
+              this.checked = false;
+              booRadio = null;
+          }else{
+              booRadio = this;
+          }
+      };
+  }
+
+  // Radio Button Unmark 2
+  var allRadios = document.getElementsByName('test');
   var booRadio;
   var x = 0;
   for(x = 0; x < allRadios.length; x++){
