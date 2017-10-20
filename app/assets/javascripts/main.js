4862578e2583667c84ee7ce730ed79ea3f5d35f1
Yuki Finamore <yuki@loldesign.com.br>
2017-10-20 19:06:25 -0200

    Added js to unmark radio button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,18 @@
-$(function() {
-  $( "#sortable1, #sortable2, #sortable3" ).sortable({
-     connectWith: "#sortable1, #sortable2, #sortable3",
-  });  
-  $( "#sortable1, #sortable2, #sortable3" ).disableSelection();   
-});
+$(document).on('ready page:load', function() {
+  var allRadios = document.getElementsByName('re');
+  var booRadio;
+  var x = 0;
+  for(x = 0; x < allRadios.length; x++){
 
+      allRadios[x].onclick = function() {
+          if(booRadio == this){
+              this.checked = false;
+              booRadio = null;
+          }else{
+              booRadio = this;
+          }
+      };
+  }
+}); 
 
 
