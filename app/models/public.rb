d00cd5dfe4025e4b605908594d4c92daa9952492
Yuki Finamore <yuki@loldesign.com.br>
2017-11-10 09:10:54 -0200

    front of many pages

--- app/models/public.rb
+++ app/models/public.rb
@@ -2,7 +2,8 @@ class Public < ApplicationRecord
   extend Enumerize
   belongs_to :project
 
-  enumerize :gender, in: [:female, :male]
+  enumerize :gender, in: [:masculino, :feminino, :outro]
   enumerize :schooling, in: [:fundamental, :medium, :superior, :post_graduated, :doctorated, :master]
   enumerize :locale, in: [:north, :northeast, :midwest, :southeast, :south]
+  enumerize :kind_of_disability, in: [:cegueira, :baixa_visão, :surdez, :deficiência_auditiva, :deficiência_Física_Leve, :deficiência_Física_Severa, :outras]
 end
